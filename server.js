b6184979c7711b635b07f4ecc2329c14f888eaca
Rachel <rachelmmarriott@gmail.com>
2022-03-03 21:17:48 -0700

    handle note creation

--- server.js
+++ server.js
@@ -1,20 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
 const { notes } = require('./db/db.json');
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-function filterByQuery(query, notesArray) {
-    let filteredResults = notesArray;
-    if (query.title) {
-        filteredResults = filteredResults.filter(note => note.title === query.title);
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+function createNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return note;
+}
+
+function validateNote(note) {
+    if(!note.title || typeof note.title !== 'string') {
+        return false;
     }
+    if(!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
 }
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body in incorrect, send 400 error 
+    if (!validateNote(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    // add note to json file and notes array
+    const note = createNote(req.body, notes);
+    res.json(note);
+    }
 });
 
 app.listen(PORT, () => {
