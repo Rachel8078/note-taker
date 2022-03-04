b15ff5a6ce018404b151196877e677a8d3e0e348
Rachel <rachelmmarriott@gmail.com>
2022-03-03 20:41:57 -0700

    update PORT info

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { notes } = require('./db/db.json');
 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, notesArray) {
@@ -16,6 +17,6 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
