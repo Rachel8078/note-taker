f114bfcc2d7b2a244c69f3f0e4ecea6641fbdfeb
Rachel <rachelmmarriott@gmail.com>
2022-03-06 19:18:15 -0700

    add delete note

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const { notes } = require('./db/db.json');
-
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// create notes
+// Create a note
 function createNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -26,7 +24,7 @@ function createNote(body, notesArray) {
     return note;
 }
 
-// validate notes
+// Validate that notes have a title and text content
 function validateNote(note) {
     if(!note.title || typeof note.title !== 'string') {
         return false;
@@ -37,20 +35,25 @@ function validateNote(note) {
     return true;
 }
 
+// Load all previous notes
 app.get('/api/notes', (req, res) => {
-    let results = notes;
+    const { notes } = JSON.parse(fs.readFileSync(
+        path.join(__dirname, './db/db.json')))
     console.log(req.query)
-    res.json(results);
+    res.json(notes);
 });
 
+// Create a note
 app.post('/api/notes', (req, res) => {
+    const { notes } = JSON.parse(fs.readFileSync(
+        path.join(__dirname, './db/db.json')))
     // req.body is where incoming content will be
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
 
     // if any data in req.body in incorrect, send 400 error 
     if (!validateNote(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The note is not properly formatted.');
     } else {
     // add note to json file and notes array
     const note = createNote(req.body, notes);
@@ -66,10 +69,25 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// handle wildcard routes
+// Send wildcard routes to homepage
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    const { notes } = JSON.parse(fs.readFileSync(
+        path.join(__dirname, './db/db.json')))
+    const filteredNotes = 
+    notes.filter(note => 
+        note.id !== req.params.id
+    );
+        console.log(filteredNotes);
+        fs.writeFileSync(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify({ notes: filteredNotes }, null, 2)
+        );
+        res.json(filteredNotes);
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
